8e7634f85ccf4f104af14f5565ae16abc8dab6db
solomonakinbiyi <akinbiyisolo22@gmail.com>
2023-03-25 18:34:23 +0100

    created json data for reviews

---
+++ src/data/Review.js
@@ -0,0 +1,62 @@
+export const Review = [
+    {
+        img_url: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        full_name: "John Doe",
+        content: "Thank you so much for being such a supportive and friendly presence on this platform! Your positivity and kindness always shine through in your interactions with others, and it's truly inspiring to see. Keep being amazing!",
+        ratings: 5
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        full_name: "Sarah Dave",
+        content: "Your contributions to the community are invaluable. Your thoughtful comments and insightful posts have helped to create a welcoming and inclusive space for all users. Your dedication to making this platform a better place is truly appreciated!",
+        ratings: 4
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=461&q=80",
+        full_name: "Grace Larson",
+        content: "You have a real talent for creating engaging and informative content that is both entertaining and educational. Your posts and articles are always well-researched and thoughtfully presented. Keep up the great work!",
+        ratings: 3
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        full_name: "Christopher James",
+        content: "Your sense of humor and wit never fail to brighten up my day. You have a real gift for making others laugh and bringing a smile to their face. Thank you for sharing your humor and positivity with us all!",
+        ratings: 4
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        full_name: "Baetrice Peters",
+        content: " Your generosity and kindness know no bounds. You are always there to offer support and encouragement to those who need it, and your willingness to help others is truly admirable. Thank you for being such an amazing person!",
+        ratings: 5
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        full_name: "Jimmy Sponge",
+        content: "Your creativity and talent are truly inspiring. Your artwork and designs are always top-notch, and your ability to bring your ideas to life is truly impressive. Keep sharing your talents with the world!",
+        ratings: 5
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+        full_name: "Franks David",
+        content: "Your passion for learning and self-improvement is contagious. Your dedication to personal growth and development is truly inspiring, and I have no doubt that you will achieve great things in life. Keep up the amazing work!",
+        ratings: 2
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1640951613773-54706e06851d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        full_name: "Rahu Sonny",
+        content: "Your leadership skills are second to none. You have a real talent for bringing people together and motivating them to achieve their goals. Your ability to lead by example is truly impressive, and I have no doubt that you will continue to make a positive impact on the world.",
+        ratings: 5
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        full_name: "Kirk Franklin",
+        content: "Your positivity and enthusiasm are infectious. Your energy and passion for life are truly inspiring, and it's always a pleasure to interact with you. Keep spreading joy and positivity wherever you go!",
+        ratings: 1
+    },
+    {
+        img_url: "https://images.unsplash.com/photo-1569913486515-b74bf7751574?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80",
+        full_name: "Ruth Tiger",
+        content: "Your intelligence and knowledge are truly impressive. Your ability to articulate complex ideas in a clear and concise way is a real gift, and your insights and opinions are always thought-provoking. Thank you for sharing your expertise with us all!",
+        ratings: 5
+    },
+]
